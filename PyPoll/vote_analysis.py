38a9a23f0d4fb7a9dc0b81e4e8c5d605332bf6e4
Pradanthony <125171223+Pradanthony@users.noreply.github.com>
2023-03-09 22:04:39 -0500

    Update vote_analysis.py

--- PyPoll/vote_analysis.py
+++ PyPoll/vote_analysis.py
@@ -2,6 +2,7 @@ import csv
 
 votes_data="Resources/election_data.csv"
 row_count=0
+ind_vote_count={}
 
 # Open the CSV file
 with open(votes_data,'r') as csvfile:
@@ -13,9 +14,24 @@ with open(votes_data,'r') as csvfile:
     # row count for months
     for row in reader:
         row_count +=1
+        candidate=row[2]
+
+        if candidate in ind_vote_count:
+            ind_vote_count[candidate]+=1
+
+        else:
+            ind_vote_count[candidate]=1
    
 #print the analysis results
 print("Election Results")
 print("_________________________________")
 print("Total Votes:",row_count)
 print("_________________________________")
+
+for candidate, votes in ind_vote_count.items():
+    print(f"{candidate}:{round(votes/row_count*100,3)}% ({votes})")
+
+print("_________________________________")
+
+winner=max(ind_vote_count)
+print(winner)
